4cdea004ce9668b8fe7e393ab366abd09cb9d071
Stephen Lewis <stephenrlewis@google.com>
2022-03-10 10:55:49 -0800

    Reverted removal of Utils section from gcptarget library

--- pkg/gcptarget/library.go
+++ pkg/gcptarget/library.go
@@ -59,6 +59,37 @@ autoreject_review[rejection] {
 path_matches(path, pattern) {
 	glob.match(pattern, ["/"], path)
 }
+
+########
+# Util #
+########
+# get_default returns the value of an object's field or the provided default value.
+# It avoids creating an undefined state when trying to access an object attribute that does
+# not exist
+get_default(object, field, _default) = output {
+  has_field(object, field)
+  output = object[field]
+}
+
+get_default(object, field, _default) = output {
+  has_field(object, field) == false
+  output = _default
+}
+
+# has_field returns whether an object has a field
+has_field(object, field) = true {
+  object[field]
+}
+# False is a tricky special case, as false responses would create an undefined document unless
+# they are explicitly tested for
+has_field(object, field) = true {
+  object[field] == false
+}
+has_field(object, field) = false {
+  not object[field]
+  not object[field] == false
+}
+
 `
 
 var libraryTemplate = template.Must(template.New("Library").Parse(libraryTemplateSrc))
